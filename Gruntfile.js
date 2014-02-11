417be9df29c203dcd392f7c24143d1f62e0ff003
Jean-Marc Nicoux <jm.nicoux@moodmedia.com>
2014-02-11 14:45:41 +0100

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+/*global module:false*/
+module.exports = function(grunt) {
+  require('time-grunt')(grunt);
+  // load all grunt tasks
+  require('load-grunt-tasks')(grunt);
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+    'sftp-put': {
+    },
+
+  });
+
+  grunt.registerTask('deploy', function (target) {
+    var deploy = process.env.DEPLOY && process.env.DEPLOY || '',
+    config;
+    target = target && target || '';
+    if ( deploy === '' ) {
+      return false;
+    }l
+    var sshlist = grunt.file.readJSON('../.' + deploy + '_config.json'));
+    if ( !sshlist ) {
+      return false;
+    }
+
+    if ( target !== '' && sshlist.sftp_put && sshlist.sftp_put[target]) {
+      grunt.config.set('sftp-put', sshlist.sftp_put[target]);
+      return grunt.task.run('sftp-put:'+ target);
+    } else if ( target !== '' ) {
+      return false;
+    }
+
+    if( !sshlist.sftp_put ) {
+      return false;
+    }
+    grunt.config.set('sftp-put', sshlist.sftp_put);
+    config = Object.keys(sshlist.sftp_put);
+    if ( !config ) {
+      return false;
+    }
+    config.forEach(function(value){
+      grunt.task.run('sftp-put:' + value);
+    });
+  });
+  // Default task.
+  grunt.registerTask('default', ['deploy']);
+};
