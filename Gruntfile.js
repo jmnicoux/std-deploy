d7f952a2beaa67a8a8bfb07e0edad95752484c67
Jean-Marc Nicoux <jm.nicoux@moodmedia.com>
2014-02-12 09:37:37 +0100

    add multiple tasks by configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,14 +8,15 @@ module.exports = function(grunt) {
     // Task configuration.
     'sftp-deploy': {
     },
-
+    sshexec: {
+    }
   });
 
   grunt.registerTask('deploy', function () {
     var deploy = process.env.DEPLOY && process.env.DEPLOY || '',
     build_env = process.env.BUILD_ENV && process.env.BUILD_ENV || 'deployment',
     target = process.env.TARGET && process.env.TARGET || '',
-    config;
+    configs, elements;
     if ( deploy === '' ) {
       return false;
     }
@@ -23,24 +24,32 @@ module.exports = function(grunt) {
     if ( !sshlist ) {
       return false;
     }
-
-    if ( target !== '' && sshlist.sftp_deploy && sshlist.sftp_deploy[target]) {
-      grunt.config.set('sftp-deploy', sshlist.sftp_deploy[target]);
-      return grunt.task.run('sftp-deploy:'+ target);
+    if ( target !== '' && sshlist[target] ) {
+      configs = [ target ];
+      if( !configs ) {
+        return false;
+      }
+      elements = {};
+      elements[target] = sshlist[target];
     } else if ( target !== '' ) {
       return false;
+    } else {
+      configs = Object.keys(sshlist);
+      if ( !configs ) {
+        return false;
+      }
+      elements = sshlist;
     }
-
-    if( !sshlist.sftp_put ) {
-      return false;
-    }
-    grunt.config.set('sftp-deploy', sshlist.sftp_deploy);
-    config = Object.keys(sshlist.sftp_deploy);
-    if ( !config ) {
-      return false;
-    }
-    config.forEach(function(value){
-      grunt.task.run('sftp-deploy:' + value);
+    
+    configs.forEach(function(config) {
+      var tasks = Object.keys(elements[config]);
+      tasks.forEach(function(task) {
+        var details = Object.keys(elements[config][task])
+        grunt.config.set(task, element[config][task]);
+        details.forEach(function(value){
+          grunt.task.run(task +':' + value);
+        });
+      })
     });
   });
   // Default task.
