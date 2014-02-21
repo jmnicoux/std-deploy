954875d4aa1c51ddcfd3317c5cdbf16a83e23746
Jean-Marc Nicoux <jm.nicoux@moodmedia.com>
2014-02-21 14:19:48 +0100

    add type of deployment
    * allow to manage different actions to execute on destinations
    * add environnement variable to specify the type of deployment

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,12 +5,13 @@ module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
   var deploy = process.env.DEPLOY && process.env.DEPLOY || '',
   build_env = process.env.BUILD_ENV && process.env.BUILD_ENV || 'deployment',
+  build_type = process.env.BUILD_TYPE && process.env.BUILD_TYPE || 'package',
   actions = grunt.file.readJSON('../.' + build_env + '_actions_' + deploy + '.json');
   actions.deploy = grunt.file.readJSON('../.' + build_env + '_config_' + deploy + '.json');
 
   // Project configuration.
   grunt.initConfig(actions);
-  grunt.registerMultiTask('deploy', function () {
+  grunt.registerMultiTask('deploy:' + build_type, function () {
     var options = this.options();
     grunt.option.init(options);
     grunt.util.spawn({
