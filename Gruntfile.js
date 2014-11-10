8b3b751e4ec084216f3df90acbe0a418a94b9999
Jean-Marc Nicoux <jm.nicoux@moodmedia.com>
2014-11-10 15:38:12 +0100

    add common server configuration file use for deployment process

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,66 +8,81 @@ module.exports = function(grunt) {
   build_env = process.env.BUILD_ENV && process.env.BUILD_ENV || 'deployment',
   build_type = process.env.BUILD_TYPE && process.env.BUILD_TYPE || 'package',
   actions = {};
-  var TARGET = process.env.TARGET && process.env.TARGET || '';
-  var build_config = grunt.file.readJSON('../.' + build_env + '_actions_' + deploy + '.json');
+  var TARGET = process.env.TARGET && process.env.TARGET || null;
+  var config_file_base = '../.' + build_env;
+  var build_config = grunt.file.exists(config_file_base + '_actions_' + deploy + '.json') && grunt.file.readJSON(config_file_base + '_actions_' + deploy + '.json') || {};
+  var server_configs = grunt.file.exists(config_file_base + '_configs.json') && grunt.file.readJSON('../.' + build_env + '_configs.json') || {};
+
   actions.deploy = build_config[build_type];
   grunt.initConfig(actions);
-  grunt.registerTask('deploy:ssh-deploy', function (dst) {
-    var options = grunt.config.get('deploy.options');
-    var task = grunt.config.get('deploy.ssh-deploy:'+dst);
-    var lstserver = task.lstserver;
-    for (var index in lstserver){
-      if (lstserver.hasOwnProperty(index)) {
-        var srv = lstserver[index];
-        var hosts = options.lstserver;
-        var result = hosts.filter(function(o) {
-          if(TARGET && TARGET != "" && TARGET === srv ) {
-            return o.hasOwnProperty(srv);
-          }
-          if(!TARGET || TARGET === "") {
-            return o.hasOwnProperty(srv);
-          }
-        });
-        if(result && result[0] && result[0][srv] && result[0][srv].options) {
-          var host = result[0][srv]["options"];
-          if (TARGET) {
-              grunt.config.set('TARGET', TARGET);
-          } else {
-              grunt.config.set('TARGET', srv);
-          }
+  grunt.config.set('deploy.options.lstserver', server_configs);
+  grunt.registerTask('filterdest', function () {
+    var options = this.options(),
+      hosts = grunt.config.get(options.hosts),
+      hkeys = Object.keys(hosts),
+      host,
+      fltlist = options.fltlist,
+      result={};
+    if (options.target && options.target != "" && hosts.hasOwnProperty(options.target)) {
+      result[options.target] = hosts[options.target]; 
+      grunt.config.set('filterdest.result', result);
+      return true;
+    }
+    fltlist.map(function(hostn) {
+      if(hosts.hasOwnProperty(hostn)) {
+        result[hostn] = hosts[hostn];
+      }
+    });
+    grunt.config.set('filterdest.result', result);
+    return true;
+  });
+  grunt.registerTask('sshCmd', function() {
+    var options = this.options(),
+      result = grunt.config.get('filterdest.result'),
+      dst = options.task;
+    for (var index in result){
+      if (result.hasOwnProperty(index)) {
+        if(result[index].options) {
+          var host = result[index].options;
+          grunt.config.set('TARGET', index);
           task = grunt.config.get('deploy.ssh-deploy:'+dst);
-          var cmd = task.cmd && task.cmd || null;
-          var src = task.src && task.src || null;
-          var dest = task.dest && task.dest || null;
           if (host) {
-            var ip = host.host && host.host || null;
-            var port = host.port && host.port || "22";
-            var username = host.username && host.username || null;
-            var password = host.password && host.password || null;
-            var key = host.key && host.key || null;
-            if (!ip || !username) {
-              return;
+            if (!host.host || !host.username) {
+              return false;
             }
-            if (!password && !key) {
-              return;
+            if (!host.password && !host.key) {
+              return false;
             }
-            if (cmd) {
-              if (password) {
-                shell.exec('sshpass -p "' + password + '" ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -p ' + port + ' ' + username + '@' + ip + ' \'' + cmd + '\'' );
+            host.port = host.port && host.port || 22;
+            if (task.cmd) {
+              if (host.password) {
+                shell.exec('sshpass -p "' + host.password + '" ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -p ' + host.port + ' ' + host.username + '@' + host.host + ' \'' + task.cmd + '\'' );
               } else {
-                shell.exec('ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -i '+ key + ' -p ' + port + ' ' + username + '@' + ip + ' \'' + cmd + '\'' );
+                shell.exec('ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -i '+ host.key + ' -p ' + host.port + ' ' + host.username + '@' + host.host + ' \'' + task.cmd + '\'' );
               }
-            } else if(src && dest) {
-              if (password) {
-                shell.exec('sshpass -p "' + password + '" scp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -r -P ' + port + ' ' + src + ' ' + username + '@' + ip + ':' + dest);
+            } else if(task.src && task.dest) {
+              if (host.password) {
+                shell.exec('sshpass -p "' + host.password + '" scp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -r -P ' + host.port + ' ' + task.src + ' ' + host.username + '@' + host.host + ':' + task.dest);
               } else {
-                shell.exec('scp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -i '+ key +' -r -P ' + port + ' ' + src + ' ' + username + '@' + ip + ':' + dest);
+                shell.exec('scp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -i '+ host.key +' -r -P ' + host.port + ' ' + task.src + ' ' + host.username + '@' + host.host + ':' + task.dest);
               }
             }
           }
         }
       }
     }
+  });
+  grunt.registerTask('deploy:ssh-deploy', function (dst) {
+    var options = grunt.config.get('deploy.options');
+    var task = grunt.config.get('deploy.ssh-deploy:'+dst);
+    var lstservertask = task.lstserver;
+    if( TARGET && TARGET != "" && lstservertask.indexOf(TARGET) === -1 ) {
+      return true;
+    }
+    var hosts = options.lstserver;
+    grunt.config.set('filterdest.options', {'hosts': 'deploy.options.lstserver', 'target': TARGET, 'fltlist' : lstservertask });
+    grunt.config.set('sshCmd.options', {task:dst});
+    grunt.task.run(['filterdest', 'sshCmd']);
   });
   // Project configuration.
   grunt.registerMultiTask('deploy', function () {
